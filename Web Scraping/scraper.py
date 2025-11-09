60d3a5e213745785b0444a0ad16789d6216ec768
MG.Mohammad Iliyaz <157681384+MGMohammad-Iliyaz@users.noreply.github.com>
2025-11-09 14:21:51 +0530

    Update scraper.py

--- Web Scraping/scraper.py
+++ Web Scraping/scraper.py
@@ -21,6 +21,8 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=chrome_options)
+
+driver = webdriver.Chrome(options=chrome_options)
 def setup_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
